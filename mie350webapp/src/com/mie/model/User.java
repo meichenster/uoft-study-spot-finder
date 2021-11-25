b18c90bbd0d75470cf97bad8735bb6483b6eec81
meichenster <meischen13@gmail.com>
2021-11-24 20:34:12 -0500

    user class

---
+++ mie350webapp/src/com/mie/model/User.java
@@ -0,0 +1,96 @@
+package com.mie.model;
+
+import java.util.Date;
+
+public class User {
+	/**
+	 * This class contains all of the relevant information, and getter/setter
+	 * methods for the Student object.
+	 */
+
+	private String username;
+	private String password;
+
+	// private String email;
+	// private String firstName;
+	// private String lastName;
+	private String faculty;
+	private String program;
+	private int yearOfStudy;
+	private Date dateCreated;
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	// public String getFirstName() {
+	// 	return firstName;
+	// }
+
+	// public void setFirstName(String firstName) {
+	// 	this.firstName = firstName;
+	// }
+
+	// public String getLastName() {
+	// 	return lastName;
+	// }
+
+	// public void setLastName(String lastName) {
+	// 	this.lastName = lastName;
+	// }
+
+	public String getFaculty() {
+		return faculty;
+	}
+	public String getProgram() {
+		return program;
+	}
+	public int yearOfStudy() {
+		return yearOfStudy;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public void setProgram(String program) {
+		this.program = program;
+	}
+	public void setYearOfStudy(int yearOfStudy) {
+		this.yearOfStudy = yearOfStudy;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	// public String getEmail() {
+	// 	return email;
+	// }
+
+	// public void setEmail(String email) {
+	// 	this.email = email;
+	// }
+
+	@Override
+	public String toString() {
+		return "Student [username=" + username + ", faculty=" + faculty
+				+ ", program=" + program + ", year of study=" + yearOfStudy + "]";
+	}
+}
