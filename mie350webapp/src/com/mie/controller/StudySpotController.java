3fd9e31e15378e589c44e16dd0ceba00c548875e
meichenster <meischen13@gmail.com>
2021-12-05 00:48:22 -0500

    review dao work

---
+++ mie350webapp/src/com/mie/controller/StudySpotController.java
@@ -0,0 +1,21 @@
+package com.mie.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mie.model.*;
+import com.mie.dao.*;
+
+/**
+ * Servlet implementation for LoginController.
+ * 
+ * This class handles the login servlet and assigns session attributes for users
+ * who succesfully log into the system.
+ */
+
+public class StudySpotController extends HttpServlet {
+    
+}
